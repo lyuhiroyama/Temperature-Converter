2bb065c10c51a53df6cc9ff96d4bad4961b41ed1
Lyu.Hiroyama <lyuhiroyama@gmail.com>
2023-08-15 16:59:19 +0900

    Initial Commit

---
+++ Temperature Converter/Temperature_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Temperature_ConverterApp.swift
+//  Temperature Converter
+//
+//  Created by Lyu Hiroyama on 2023/08/15.
+//
+
+import SwiftUI
+
+@main
+struct Temperature_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
