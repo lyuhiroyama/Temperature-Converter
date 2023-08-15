e77a72400a38d91ddfcc97b52629e18a0a7525d3
Lyu.Hiroyama <lyuhiroyama@gmail.com>
2023-08-15 19:15:05 +0900

    Built a temperature converter

--- Temperature Converter/ContentView.swift
+++ Temperature Converter/ContentView.swift
@@ -8,14 +8,99 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+    
+    @State private var temp: Double = 0
+    @State private var inputTypeSelected = "Farenheit"
+    @State private var outputTypeSelected = "Farenheit"
+    @FocusState private var tempIsFocused: Bool
+    
+    var tempTypes = ["Celcius", "Farenheit", "Kelvin"]
+    
+    var outputTemp: String {
+        var inputToCelcius: Double
+        var celciusToOutput: Double
+        
+        switch inputTypeSelected {
+        case "Farenheit":
+            inputToCelcius = (temp - 32) * (5/9)
+        case "Kelvin":
+            inputToCelcius = temp - 273.15
+        default:
+            inputToCelcius = temp
         }
-        .padding()
+        
+        switch outputTypeSelected {
+        case "Farenheit":
+            celciusToOutput = (inputToCelcius * (9/5)) + 32
+        case "Kelvin":
+            celciusToOutput = inputToCelcius + 273.15
+        default:
+            celciusToOutput = inputToCelcius * 1
+        }
+        
+        let outputValue = round(celciusToOutput * 100) / 100.0
+        
+        return "\(outputValue)  \(outputTypeSelected)"
+        
+    }
+
+    var body: some View {
+
+        NavigationView {
+            Form {
+                
+                Section {
+                    
+                    Picker("Select Type: ", selection: $inputTypeSelected) {
+                        ForEach(tempTypes, id: \.self) { option in
+                            Text(option)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                    
+                    TextField("Yomama", value: $temp, format: .number)
+                        .keyboardType(.decimalPad)
+                        .focused($tempIsFocused)
+                    
+                } header: {
+                    Text("Enter Inuput Temperature:")
+                }
+                
+                
+                Section {
+                    
+                    Picker("Select Type: ", selection: $outputTypeSelected) {
+                        ForEach(tempTypes, id: \.self) { option in
+                            Text(option)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                    
+                } header: {
+                    Text("Select output type:")
+                }
+                
+                
+                
+                Section {
+                    Text(outputTemp)
+                } header: {
+                    Text("Output:")
+                }
+                
+                
+            }
+            .navigationBarTitle("Temperature Converter")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    Button("Done"){
+                        tempIsFocused = false
+                    }
+                }
+            }
+        }
+        
     }
 }
 
